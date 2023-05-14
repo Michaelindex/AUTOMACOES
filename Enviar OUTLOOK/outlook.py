714fbbd8c76af4e91ee409d95bd4a5ef35a2d245
Michael Santos <mcialbr123@gmail.com>
2023-05-13 22:50:13 -0300

    enviar OUTLOOK

---
+++ Enviar OUTLOOK/outlook.py
@@ -0,0 +1,35 @@
+#AQUI APENAS UM EMAIL CONVENCIONAL
+
+#import win32com.client as win32
+#
+#outlook = win32.Dispatch("Outlook.Application")
+#message = outlook.CreateItem(0)
+#message.Display()
+#message.To = "mcialbr123@gmail.com"
+#message.Subject = "AQUI O ASSUNTO"
+#body = """
+#<h1>Michael</h1>
+#<p>Teste Teste</p>
+#"""
+#
+#message.HTMLBody = body
+
+
+#AQUI DISPARANDO PARA VAIOS EMAILS EM lista_email
+
+import win32com.client as win32
+outlook = win32.Dispatch("Outlook.Application")
+
+lista_email = ['mcialbr123@gmail.com', 'mcialbr1234@gmail.com', 'mcialbr1234@hotmail.com']
+
+for email in lista_email:
+    message = outlook.CreateItem(0)
+    message.Display()
+    message.To = email
+    message.Subject = "AQUI O ASSUNTO"
+    body = """
+    <h1>Michael</h1>
+    <p>Teste Teste</p>
+    """
+
+    message.HTMLBody = body
