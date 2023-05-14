525f7d4c9ded423ec7d675a6d08fb514e5a9b483
Michael Santos <mcialbr123@gmail.com>
2023-05-13 22:20:37 -0300

    automacao p GMAIL

---
+++ Enviar GMAIL/gmail.py
@@ -0,0 +1,28 @@
+import smtplib
+import email.message
+
+def enviar_email():  
+    corpo_email = """
+    <h1>Boa Tarde, Michael aqui</h1>
+    <h3>REFERENTE A EMPRESA X </h3>
+    <p>Segue notas fiscais da empresa X com a numeracao de X</p>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "testeteste"
+    msg['From'] = 'mcialbr123@gmail.com'
+    msg['To'] = 'mcialbr123@gmail.com'
+    password = 'MINHA-SENHA'
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
+
+
+enviar_email()
+
