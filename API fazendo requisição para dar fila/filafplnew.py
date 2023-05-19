3cd8bd6c6570af0ebc2db13497e472d5d757e4ea
Michael Santos <mcialbr123@gmail.com>
2023-05-19 00:48:39 -0300

    Create filafplnew.py

---
+++ API fazendo requisição para dar fila/filafplnew.py
@@ -0,0 +1,39 @@
+import json
+import requests
+import time
+
+start_time = time.time()
+
+while time.time() - start_time < 60:
+    # Set the API endpoint
+    url = "https://api.faceit.com/queue/v1/player/633f0132403c133d88b9832b"
+
+    # Set the request headers
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer b086711b-eccf-43b3-9da9-d1bef2fc604b"
+    }
+
+    # Set the request data
+    data = {
+        "leaderId": "ee4d0675-d8db-43ea-8137-390f4a9277d0",
+        "playerId": "ee4d0675-d8db-43ea-8137-390f4a9277d0",
+        "playerType": "solo",
+        "userIds" : ["ee4d0675-d8db-43ea-8137-390f4a9277d0"],
+        "0" : "ee4d0675-d8db-43ea-8137-390f4a9277d0"
+
+    }
+
+    # Convert the data to json format
+    payload = json.dumps(data)
+
+    # Make the request
+    response = requests.post('https://api.faceit.com/queue/v1/player/633f0132403c133d88b9832b', headers=headers, data=payload)
+
+    # Print the response status code
+    print(response.status_code)
+
+    # Print the response content
+    print(response.json())
+
+    time.sleep(0.1)
