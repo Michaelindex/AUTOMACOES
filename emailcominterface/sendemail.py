c23868fe1554d7e18889f528a2dfa8a576a10677
Michael Santos <mcialbr123@gmail.com>
2023-05-30 20:42:07 -0300

    commited

--- emailcominterface/sendemail.py
+++ emailcominterface/sendemail.py
@@ -3,30 +3,32 @@ from tkinter import filedialog
 import win32com.client as win32
 
 def selecionar_anexo():
+    # Função para selecionar o arquivo de anexo
     filename = filedialog.askopenfilename()
-    entry_anexo.delete(0, tk.END)
-    entry_anexo.insert(0, filename)
+    entry_anexo.delete(0, tk.END)  # Limpa o campo de entrada do anexo
+    entry_anexo.insert(0, filename)  # Insere o nome do arquivo selecionado no campo de entrada
 
 def enviar_email():
+    # Função para enviar os e-mails
     outlook = win32.Dispatch("Outlook.Application")
 
-    lista_email = entry_emails.get().split(',')
-    assunto = entry_assunto.get()
-    corpo = entry_corpo.get("1.0", "end-1c")
-    anexo = entry_anexo.get()
+    lista_email = entry_emails.get().split(',')  # Obtém a lista de e-mails do campo de entrada
+    assunto = entry_assunto.get()  # Obtém o assunto do e-mail do campo de entrada
+    corpo = entry_corpo.get("1.0", "end-1c")  # Obtém o corpo do e-mail do campo de entrada de texto
+    anexo = entry_anexo.get()  # Obtém o caminho do anexo do campo de entrada
 
     for email in lista_email:
         message = outlook.CreateItem(0)
         message.To = email
         message.Subject = assunto
         message.Body = corpo
-        
-        if anexo:
-            message.Attachments.Add(anexo)
-        
-        message.Send()
 
-    label_status["text"] = "E-mails enviados com sucesso!"
+        if anexo:
+            message.Attachments.Add(anexo)  # Adiciona o anexo ao e-mail, se houver
+
+        message.Send()  # Envia o e-mail
+
+    label_status["text"] = "E-mails enviados com sucesso!"  # Atualiza a etiqueta de status
 
 # Cria a janela principal
 window = tk.Tk()
@@ -34,33 +36,33 @@ window.title("Enviar E-mails")
 window.geometry("400x400")
 
 # Cria os widgets
-label_emails = tk.Label(window, text="E-mails:")
+label_emails = tk.Label(window, text="E-mails:")  # Rótulo para os e-mails
 label_emails.pack()
-entry_emails = tk.Entry(window, width=40)
+entry_emails = tk.Entry(window, width=40)  # Campo de entrada para os e-mails
 entry_emails.pack()
 
-label_assunto = tk.Label(window, text="Assunto:")
+label_assunto = tk.Label(window, text="Assunto:")  # Rótulo para o assunto
 label_assunto.pack()
-entry_assunto = tk.Entry(window, width=40)
+entry_assunto = tk.Entry(window, width=40)  # Campo de entrada para o assunto
 entry_assunto.pack()
 
-label_corpo = tk.Label(window, text="Corpo:")
+label_corpo = tk.Label(window, text="Corpo:")  # Rótulo para o corpo do e-mail
 label_corpo.pack()
-entry_corpo = tk.Text(window, width=40, height=6)
+entry_corpo = tk.Text(window, width=40, height=6)  # Campo de entrada de texto para o corpo do e-mail
 entry_corpo.pack()
 
-label_anexo = tk.Label(window, text="Anexo:")
+label_anexo = tk.Label(window, text="Anexo:")  # Rótulo para o anexo
 label_anexo.pack()
-entry_anexo = tk.Entry(window, width=30)
+entry_anexo = tk.Entry(window, width=30)  # Campo de entrada para o caminho do anexo
 entry_anexo.pack()
 
-button_selecionar_anexo = tk.Button(window, text="Selecionar Anexo", command=selecionar_anexo)
+button_selecionar_anexo = tk.Button(window, text="Selecionar Anexo", command=selecionar_anexo)  # Botão para selecionar o anexo
 button_selecionar_anexo.pack()
 
-button_enviar = tk.Button(window, text="Enviar", command=enviar_email)
+button_enviar = tk.Button(window, text="Enviar", command=enviar_email)  # Botão para enviar os e-mails
 button_enviar.pack()
 
-label_status = tk.Label(window, text="")
+label_status = tk.Label(window, text="")  # Etiqueta para exibir o status do envio dos e-mails
 label_status.pack()
 
 # Inicia o loop principal da janela
