3ddb33c9f061de1a1c990bb98439cfde26849b6e
Michael Santos <mcialbr123@gmail.com>
2023-06-16 23:02:57 -0300

    Update gmail.py

--- Enviar GMAIL/gmail.py
+++ Enviar GMAIL/gmail.py
@@ -9,7 +9,7 @@ def enviar_email():
     """
 
     msg = email.message.Message()
-    msg['Subject'] = "teste"
+    msg['Subject'] = ""
     msg['From'] = 'mcialbr123@gmail.com'
     msg['To'] = 'mcialbr123@gmail.com'
     password = 'MINHA-SENHA'
