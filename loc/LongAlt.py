06a61332ee71dcd8a4e02f8ddf6fd4ca283c8136
Michael Santos <mcialbr123@gmail.com>
2023-06-17 22:49:45 -0300

    Update LongAlt.py

--- loc/LongAlt.py
+++ loc/LongAlt.py
@@ -5,12 +5,12 @@ from geopy.geocoders import Nominatim
 # Função para obter a localização atual
 def get_location():
     geolocator = Nominatim(user_agent='my_location')
-    location = geolocator.geocode('')
+    location = geolocator.geocode('NomeDaRua, Número, Cidade, Estado, País')
     return location
 
 # Função para enviar mensagem para o bot do Telegram
 def send_message(chat_id, text):
-    bot_token = '6258166876:AAHFOyYvt0zKgyLQSBYQ54bhYbpnq_79NlQ'
+    bot_token = 'SEU_TOKEN_DO_BOT'
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     payload = {
         'chat_id': chat_id,
@@ -27,12 +27,14 @@ def main():
             if location:
                 latitude = location.latitude
                 longitude = location.longitude
-                chat_id = '5212651576'  # Substitua pelo ID do chat do Telegram
+                chat_id = 'SEU_CHAT_ID'  # Substitua pelo ID do chat do Telegram
                 message = f'Localização atual: Latitude {latitude}, Longitude {longitude}'
+                print(message)  # Exibe a mensagem no terminal
                 send_message(chat_id, message)
         except Exception as e:
             print(f'Erro: {str(e)}')
 
+        time.sleep(3600)
 
 if __name__ == '__main__':
     main()
