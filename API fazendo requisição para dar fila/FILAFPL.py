7fefec3ce22747e6cb1e06eeb1c121d5ad482e23
Michael Santos <mcialbr123@gmail.com>
2023-06-21 22:38:13 -0300

    att

--- API fazendo requisição para dar fila/FILAFPL.py
+++ API fazendo requisição para dar fila/FILAFPL.py
@@ -6,21 +6,23 @@ start_time = time.time()
 
 while time.time() - start_time < 60:
     # Set the API endpoint
-    url = "#"
+    url = "https://api.faceit.com/queue/v2/player"
 
     # Set the request headers
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer #"
+        "Authorization": "Bearer 64193c63-1f06-4dc7-b36d-99061618029b"
     }
 
     # Set the request data
     data = {
-        "leaderId": "#",
-        "playerId": "#",
+        "entityId" : "c1686bfd-3737-4e61-93b8-474b4e48d059",
+        "entityType" : "hub",
+        "leaderId": "f4f8db79-0e8a-4a12-a2f0-fee9537219e7",
+        "playerId": "f4f8db79-0e8a-4a12-a2f0-fee9537219e7",
         "playerType": "solo",
-        "userIds" : ["#"],
-        "0" : "#"
+        "userIds" : ["f4f8db79-0e8a-4a12-a2f0-fee9537219e7"],
+        "0" : "f4f8db79-0e8a-4a12-a2f0-fee9537219e7"
 
     }
 
@@ -28,7 +30,7 @@ while time.time() - start_time < 60:
     payload = json.dumps(data)
 
     # Make the request
-    response = requests.post('#', headers=headers, data=payload)
+    response = requests.post('https://api.faceit.com/queue/v2/player', headers=headers, data=payload)
 
     # Print the response status code
     print(response.status_code)
