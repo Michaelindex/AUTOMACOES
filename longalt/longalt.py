928ca427ae2ecdc9638b73cba34041fde9315b43
Michael Santos <mcialbr123@gmail.com>
2023-06-27 23:02:34 -0300

    Delete longalt.py

--- longalt/longalt.py
+++
@@ -1,6 +0,0 @@
-from geopy.geocoders import Nominatim
-
-geolacalizador = Nominatim(user_agent="teste-palancacode")
-lugar_input = input("Digite o nome do lugar: ")
-lacalizacao = geolacalizador.geocode(lugar_input)
-print(lacalizacao)
